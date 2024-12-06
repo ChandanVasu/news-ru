07d2678915c2f3c43af6276e289b0790472b68cb
chandanyadavvasu <chandanyadavvasu@gmail.com>
2024-12-06 19:47:26 +0530

    fix deplay

--- src/components/categoryList.jsx
+++ src/components/categoryList.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { use } from "react";
+import React from "react";
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import { categoryNameMapping } from "@/models/translations";
